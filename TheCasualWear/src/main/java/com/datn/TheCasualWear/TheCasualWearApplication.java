3dbc529b6c05edae320fc6502634ae45994a2384
tiinnnn <imdino913@gmail.com>
2026-03-24 14:48:01 +0700

    init

---
+++ TheCasualWear/src/main/java/com/datn/TheCasualWear/TheCasualWearApplication.java
@@ -0,0 +1,13 @@
+package com.datn.TheCasualWear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCasualWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCasualWearApplication.class, args);
+	}
+
+}
---
+++ TheCasualWear/src/test/java/com/datn/TheCasualWear/TheCasualWearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datn.TheCasualWear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCasualWearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
